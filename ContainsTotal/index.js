246b6e31c4cf105b6ab1391948dffbf14b44d3b8
AliPort <101525919+AliPort@users.noreply.github.com>
2022-11-28 21:40:37 -0600

    working on ContainsTotal

--- ContainsTotal/index.js
+++ ContainsTotal/index.js
@@ -1,17 +1,24 @@
 // Write a function that will tell you if there are 2 items in the array that add up to the target number
-// Rules: 
+// Rules:
 // A number can only be used once per entry
 // However the list may contain repeats
 
-// Examples: 
+// Examples:
 // containsTotal([1,2,3], 5) => true
 // containsTotal([1,2,3], 6) => false
 // containsTotal([1,3], 2) => false (the number 1 can only be used one time)
 // containsTotal([1,2,1], 2) => true (1 + 1 = 2)
 
 const containsTotal = (numArr, target) => {
-  return null
+  for (let i = 0; i < numArr.length; i++) {
+    for (let j = 1; j < numArr.length; j++) {
+      if (numArr[i] + numArr[j] == target) 
+        return true;
+      else {return false}
+      console.log(numArr[(i+j)]);
+      
+    }
+  }
 };
-
 
 module.exports = containsTotal;
